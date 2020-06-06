4c4807b19991d52044c4e589e3b8093fd26604a1
Plamena Marinova <plamena.marinova@gmail.com>
2020-06-06 16:40:12 +0300

    added settings activity

---
+++ app/src/main/java/pm/android/photoscreensaver/SettingsFragment.java
@@ -0,0 +1,65 @@
+package pm.android.photoscreensaver;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.leanback.preference.LeanbackPreferenceFragment;
+import androidx.leanback.preference.LeanbackSettingsFragment;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceScreen;
+
+import java.util.Arrays;
+
+public class SettingsFragment extends LeanbackSettingsFragment {
+    @Override
+    public void onPreferenceStartInitialScreen() {
+        startPreferenceFragment(buildPreferenceFragment(R.xml.prefs, null));
+    }
+
+    @Override
+    public boolean onPreferenceStartFragment(PreferenceFragment caller, Preference pref) {
+        return false;
+    }
+
+    @Override
+    public boolean onPreferenceStartScreen(PreferenceFragment caller, PreferenceScreen pref) {
+        startPreferenceFragment(buildPreferenceFragment(R.xml.prefs, pref.getKey()));
+        return true;
+    }
+
+    private PreferenceFragment buildPreferenceFragment(int preferenceResId, String root) {
+        PreferenceFragment fragment = new PrefFragment();
+        Bundle args = new Bundle();
+        args.putInt("preferenceResource", preferenceResId);
+        args.putString("root", root);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+
+    public static class PrefFragment extends LeanbackPreferenceFragment {
+
+        @Override
+        public void onCreatePreferences(Bundle bundle, String s) {
+            String root = getArguments().getString("root", null);
+            int prefResId = getArguments().getInt("preferenceResource");
+            if (root == null) {
+                addPreferencesFromResource(prefResId);
+            } else {
+                setPreferencesFromResource(prefResId, root);
+            }
+        }
+
+        @Override
+        public boolean onPreferenceTreeClick(Preference preference) {
+            final String[] keys = { "pref_key_server_host", "pref_key_server_port" };
+            if (Arrays.asList(keys).contains(preference.getKey())) {
+                Toast.makeText(getActivity(), "Implement your own action handler.", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            return super.onPreferenceTreeClick(preference);
+        }
+    }
+}
