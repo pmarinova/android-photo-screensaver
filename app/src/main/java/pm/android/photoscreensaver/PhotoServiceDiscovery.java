72aebcc8a106e08367e912e1a2b9a121df54e3ef
Plamena Marinova <plamena.marinova@gmail.com>
2023-11-01 14:40:52 +0200

    photo service discovery (wip)

---
+++ app/src/main/java/pm/android/photoscreensaver/PhotoServiceDiscovery.java
@@ -0,0 +1,76 @@
+package pm.android.photoscreensaver;
+
+import android.content.Context;
+import android.net.nsd.NsdManager;
+import android.net.nsd.NsdServiceInfo;
+import android.util.Log;
+
+public class PhotoServiceDiscovery implements NsdManager.DiscoveryListener {
+
+    public interface Callback {
+        void onServiceFound(NsdServiceInfo serviceInfo);
+        void onServiceLost(NsdServiceInfo serviceInfo);
+    }
+
+    private static final String TAG = PhotoServiceDiscovery.class.getName();
+
+    private static final String SERVICE_TYPE = "_http._tcp";
+    private static final String SERVICE_NAME = "photo-server";
+
+    private final NsdManager nsdManager;
+    private final Callback callback;
+
+    public PhotoServiceDiscovery(Context context, Callback callback) {
+        this.nsdManager = (NsdManager)context.getSystemService(Context.NSD_SERVICE);
+        this.callback = callback;
+    }
+
+    public void start() {
+        nsdManager.discoverServices(SERVICE_TYPE, NsdManager.PROTOCOL_DNS_SD, this);
+    }
+
+    public void stop() {
+        nsdManager.stopServiceDiscovery(this);
+    }
+
+    @Override
+    public void onDiscoveryStarted(String serviceType) {
+        Log.d(TAG, "service discovery started");
+    }
+
+    @Override
+    public void onDiscoveryStopped(String serviceType) {
+        Log.d(TAG, "service discovery stopped");
+    }
+
+    @Override
+    public void onStartDiscoveryFailed(String serviceType, int errorCode) {
+        Log.d(TAG, "service discovery start failed with error code " + errorCode);
+    }
+
+    @Override
+    public void onStopDiscoveryFailed(String serviceType, int errorCode) {
+        Log.d(TAG, "service discovery stop failed with error code " + errorCode);
+    }
+
+    @Override
+    public void onServiceFound(NsdServiceInfo serviceInfo) {
+        if (serviceInfo.getServiceName().equals(SERVICE_NAME)) {
+            nsdManager.resolveService(serviceInfo, new NsdManager.ResolveListener() {
+                @Override
+                public void onServiceResolved(NsdServiceInfo serviceInfo) {
+                    callback.onServiceFound(serviceInfo);
+                }
+                @Override
+                public void onResolveFailed(NsdServiceInfo serviceInfo, int errorCode) {
+                    Log.d(TAG, "service resolve failed with error code " + errorCode);
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onServiceLost(NsdServiceInfo serviceInfo) {
+        callback.onServiceLost(serviceInfo);
+    }
+}
--- app/src/main/java/pm/android/photoscreensaver/PrefFragment.java
+++ app/src/main/java/pm/android/photoscreensaver/PrefFragment.java
@@ -1,21 +1,26 @@
 package pm.android.photoscreensaver;
 
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.nsd.NsdManager;
+import android.net.nsd.NsdServiceInfo;
 import android.os.Bundle;
 
 import androidx.leanback.preference.LeanbackPreferenceFragment;
 import androidx.preference.EditTextPreference;
 import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceGroup;
 import androidx.preference.PreferenceManager;
 
 public class PrefFragment
         extends LeanbackPreferenceFragment
-        implements SharedPreferences.OnSharedPreferenceChangeListener {
+        implements SharedPreferences.OnSharedPreferenceChangeListener, PhotoServiceDiscovery.Callback {
 
     private static final String TAG = PrefFragment.class.getName();
 
     private SharedPreferences prefs;
+    private PhotoServiceDiscovery serviceDiscovery;
 
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
@@ -26,23 +31,44 @@ public class PrefFragment
         } else {
             setPreferencesFromResource(prefResId, root);
         }
+
+        serviceDiscovery = new PhotoServiceDiscovery(getPreferenceScreen().getContext(), this);
     }
 
     @Override
     public void onResume() {
         super.onResume();
-
         prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
         prefs.registerOnSharedPreferenceChangeListener(this);
-
         updateEditTextPrefSummary(getPreferenceScreen());
+        serviceDiscovery.start();
     }
 
     @Override
     public void onPause() {
+        serviceDiscovery.stop();
         prefs.unregisterOnSharedPreferenceChangeListener(this);
         prefs = null;
         super.onPause();
+    }
+
+    @Override
+    public void onServiceFound(NsdServiceInfo serviceInfo) {
+        Preference pref = new Preference(this.getPreferenceScreen().getContext());
+        pref.setTitle(serviceInfo.getServiceName());
+        pref.setSummary(serviceInfo.getHost().toString() + ":" + serviceInfo.getPort());
+
+        PreferenceCategory availableServers = findPreferenceCategory(R.string.pref_key_available_servers);
+        availableServers.addPreference(pref);
+    }
+
+    @Override
+    public void onServiceLost(NsdServiceInfo serviceInfo) {
+        //TODO
+    }
+
+    private PreferenceCategory findPreferenceCategory(int keyResId) {
+        return (PreferenceCategory)findPreference(getString(keyResId));
     }
 
     @Override
